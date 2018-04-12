423a8db6d95667fba49a974e21644c1f66686d4c
Marco Andreini <marco.andreini@gmail.com>
2018-04-12 16:57:04 +0200

    Listening not only on localhost.

--- prometheus.go
+++ prometheus.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"log"
-	"net/http"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
+  "log"
+  "net/http"
 )
 
 func prometheusHandler(prometheus bool) {
-	if prometheus {
-		http.Handle("/metrics", promhttp.Handler())
-		log.Fatal(http.ListenAndServe("localhost:8090", nil))
-	}
+  if prometheus {
+    http.Handle("/metrics", promhttp.Handler())
+    log.Fatal(http.ListenAndServe(":8090", nil))
+  }
 }
